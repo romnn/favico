7560197d1200244cc257aa1ef696c92c08c034f0
romnnn <roman.dahm@gmx.de>
2021-07-24 05:16:50 +0200

    Bump version: 0.1.7 → 0.1.8

--- favico/__init__.py
+++ favico/__init__.py
@@ -6,6 +6,6 @@ from favico.favico import generate_from_color, generate_from_image
 
 __author__ = """romnn"""
 __email__ = "contact@romnn.com"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 __all__ = ["generate_from_image", "generate_from_color"]
